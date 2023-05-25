0a447d10c4059a79992f7ad26087d7a4fa5b48f5
xkouoi <x.kouoi@tudelft.nl>
2023-05-25 16:37:45 +0200

    notified that Xavier was here

--- convert_temperature.py
+++ convert_temperature.py
@@ -6,6 +6,8 @@ def fahrenheit_to_celsius(temperature_fahrenheit):
 
      Returns:
      temperature_celsius
+
+	Xavier was here.
      '''
      temperature_celsius = (temperature_fahrenheit - 32) * (5.0/9.0)
      return temp_celsius
